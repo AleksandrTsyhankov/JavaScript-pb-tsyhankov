9372f91fd6825a00c96527a123d4d40fc0eb716d
AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
2022-01-28 20:44:12 +0200

    hw3 done

---
+++ hw3/10.js
@@ -0,0 +1,33 @@
+/*####Задача 10
+
+Создать имплементацию функции `reverse`, которая принимает массив в качестве параметра, 
+а возвращает массив только в обратном порядке.
+
+Функция должна содержать проверки:
+
+- Первый параметр обязателен и может принимать только массив
+- Генерировать ошибку если был передан пустой массив
+
+```javascript
+const arr = [3,2,1];
+reverse(arr); // [1,2,3]
+```*/
+
+function f(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('arr type should be an array');
+  };
+  if (arr.length === 0) {
+    throw new Error('arr length should be more than zero');
+  };
+
+  let newArr = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newArr.push(arr[i])
+  };
+
+  return newArr;
+};
+
+console.log(f([1,2,3,4,5]));
---
+++ hw3/9.js
@@ -0,0 +1,27 @@
+/*####Задача 9
+
+Сделайте функцию `arrayFill`, которая будет заполнять массив заданными значениями. 
+Первым параметром функция принимает значение, которым заполнять массив, 
+а вторым — сколько элементов должно быть в массиве.
+
+Функция должна содержать проверки:
+
+- Первый параметр обязателен и может принимать только число, строку, объект, массив
+- Второй параметр обязателен и может принимать только число
+
+```javascript
+arrayFill('x',5); // [x,x,x,x,x]
+```*/
+
+function arrayFill(elem, newLen) {
+if (typeof elem !== 'number' && typeof elem !== 'string' && typeof elem !== 'object') {
+      throw new Error('arr type should be an number, string or object');
+    }
+
+  let arr = [];
+  arr.length = newLen;
+  
+  return arr.fill(elem);
+}
+
+console.log(arrayFill('x', 5));
